af4383a62a9831a21153395cfc815ffa0717fd5d
Wyndale <lapurawendel.dev@gmail.com>
2025-11-07 11:09:02 +0800

    feat: initial project setup using spring boot for transparify with dependencies

---
+++ src/main/java/com/transparify/transparify_campus_ledger/TransparifyCampusLedgerApplication.java
@@ -0,0 +1,13 @@
+package com.transparify.transparify_campus_ledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransparifyCampusLedgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransparifyCampusLedgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transparify/transparify_campus_ledger/TransparifyCampusLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transparify.transparify_campus_ledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransparifyCampusLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
